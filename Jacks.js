818166248d32112e84d600b64f5f35243144411e
MatthyPlayz <cjmatthy09@gmail.com>
2018-09-15 08:48:48 -0400

    Added fill color and stroke color.
    added function canvas.fillColor and canvas.strokeColor

--- Jacks.js
+++ Jacks.js
@@ -7,5 +7,11 @@ function canvasStart(idOfCanvas, dimensions) {
 var canvas = {
 	rectangle: function rectangle(left, top, width, height) {
 		ctx.fillRect(arguments[0], arguments[1], arguments[2], arguments[3]);
-	}
+	},
+    fillColor: function fillColor(hexcolor) {
+      ctx.fillStyle=arguments[0];
+    }, 
+    strokeColor: function strokeColor(hexcolor) {
+      ctx.strokeStyle=arguments[0];
+    }
 }
